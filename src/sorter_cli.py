b72372bc99d8a342946af037e860edf3ef891809
Michael Loomis <63444134+michaelloomis@users.noreply.github.com>
2022-01-31 14:44:35 -0800

    Update sorter_cli.py
    Added to sorter_cli

--- src/sorter_cli.py
+++ src/sorter_cli.py
@@ -6,4 +6,8 @@ sort_order = sys.argv[2]
 filename = sys.argv[3]
 output_filename = sys.argv[4]
 
-# Write your code here
+Sorter(filename, sort_type, sort_order)
+if sort_type == int:
+    output_filename = sort_nums(filename)
+if sort_type == str:
+    output_filename = sort_strings(filename)
